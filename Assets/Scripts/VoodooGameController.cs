ee9ee8e61cfc253797d34c3058fbeac8ec0ca3e4
Daniel Villaverde <danielsmash94@hotmail.com>
2016-01-29 20:33:54 +0100

    Voodoo comenzado

---
+++ Assets/Scripts/VoodooGameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoodooGameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetMouseButtonDown(0)) {	
+			RaycastHit2D[] hits = new RaycastHit2D[2];
+			int num = Physics2D.GetRayIntersectionNonAlloc (Camera.main.ScreenPointToRay (Input.mousePosition), hits);
+			if (num>0)
+				foreach (RaycastHit2D hit in hits) {
+					if (hit.collider.gameObject.tag.Equals ("VoodooPoint")) {
+						Destroy (hit.collider.gameObject);
+						break;
+					}
+				}
+		}
+
+	}
+}
