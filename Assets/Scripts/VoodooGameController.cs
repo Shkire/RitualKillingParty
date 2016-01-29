6b30546bcd7b90dcd7033fb05731e9307d842ce1
Jesuskfc <gsus1946@gmail.com>
2016-01-29 21:24:00 +0100

    Prueba de click
    Lista con valores bools en los targets

--- Assets/Scripts/VoodooGameController.cs
+++ Assets/Scripts/VoodooGameController.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class VoodooGameController : MonoBehaviour {
 
+
+	[SerializeField]
+	private bool[] targetsIsClicked;
+
+	[SerializeField]
+	private GameObject[] targetsPoints;
+
 	// Use this for initialization
 	void Start () {
+
 	}
 	
 	// Update is called once per frame
@@ -14,10 +22,25 @@ public class VoodooGameController : MonoBehaviour {
 			int num = Physics2D.GetRayIntersectionNonAlloc (Camera.main.ScreenPointToRay (Input.mousePosition), hits);
 			if (num>0)
 				foreach (RaycastHit2D hit in hits) {
+					
 					if (hit.collider.gameObject.tag.Equals ("VoodooPoint")) {
-						Destroy (hit.collider.gameObject);
+						
+
+						for (int i = 0; i < (targetsPoints.Length ); i++) {
+							
+							if (hit.collider.gameObject.Equals(targetsPoints [i])) {
+								
+								targetsIsClicked [i] = true;
+								break;
+							}
+
+
+								//hit.collider.gameObject.transform.Rotate(new Vector3(0,0,45));
+								
+						}
 						break;
 					}
+
 				}
 		}
 
