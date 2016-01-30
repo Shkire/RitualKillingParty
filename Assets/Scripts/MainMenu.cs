732c890aa01da464db9a74b218aaecfc00a02483
Daniel Villaverde <danielsmash94@hotmail.com>
2016-01-30 18:05:24 +0100

    intro scene added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	[SerializeField]
+	private RitualController gameController;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		if (Input.GetKeyDown (KeyCode.Return)) {
+
+			gameController.gameObject.SetActive (true);
+			this.gameObject.SetActive (false);
+
+		}
+
+	}
+}
